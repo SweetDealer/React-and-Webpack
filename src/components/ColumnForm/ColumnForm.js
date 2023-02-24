db012c98a2fa0b67b294f31f1256c8aae37a14d5
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-02-24 15:05:38 +0200

    Added Icon Input to ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,17 +1,22 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
-    const addColumn = e => {
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: value });
-        setValue('');
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
     }
     return (
-        <form onSubmit={addColumn}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button>Add column</button>
+        <form onSubmit={handleSubmit} className={styles.columnForm}>
+            Title: <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            Icon: <TextInput type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+            <Button>Add column</Button>
         </form>
     );
 };
