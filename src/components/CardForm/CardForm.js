ea6825d7e154c4f2f7529f5bc1aa5ea13950d63f
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-03-04 22:10:34 +0200

    Changed components ColumnForm and CardForm

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,15 +2,18 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
+import shortid from 'shortid';
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
+    const dispatch = useDispatch();
+   
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { id: shortid(), title, columnId: props.columnId } });
         setTitle('');
     }
-
     return (
         <form onSubmit={handleSubmit} className={styles.cardForm}>
             <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
